8f7d2253b7229fddd0199286d72109719ae4f29e
hippasus <zhuyanggu@gmail.com>
2013-08-28 13:29:48 +0800

    add help message in home page.

--- src/main.py
+++ src/main.py
@@ -14,7 +14,8 @@ class Main(webapp2.RequestHandler):
         <style>
             body { font: 1.2em/1.2 Arial; }
             h1 { font: bold 2em/2 Verdana; }
-            p { padding: 10px 20px; font: 1em/1.5 Consolas; color: #bdbdbd; text-align: right; }
+            p.author { padding: 10px 20px; font: 1em/1.5 Consolas; color: #bdbdbd; text-align: right; }
+            .got-error { font-size: 1em; font-family: Verdana; }
         </style>
     </head>
     <body>
@@ -23,10 +24,13 @@ class Main(webapp2.RequestHandler):
             <li>Currency API: <a href="$host_url/currency?from=USD&to=EUR&q=1" target="_blank" rel="nofollow">$host_url/currency?from=USD&to=EUR&q=1</a></li>
             <li>Temperature API: <a href="$host_url/temperature?mode=C2F&q=37" target="_blank" rel="nofollow">$host_url/temperature?mode=C2F&q=37</a></li>
         </ul>
-        <div>
-            Got an error? Please help to create issue at <a href="https://github.com/hippasus/ExchangeRates/issues">https://github.com/hippasus/ExchangeRates/issues</a>.
+        <div class="got-error">
+            <i>Got an error?</i> Please help to create an issue at <a href="https://github.com/hippasus/ExchangeRates/issues">https://github.com/hippasus/ExchangeRates/issues</a>.
         </div>
-        <p>--By Hippasus Chu</p>
+        <h3>Help needed!</h3>
+        <p>We've reached the <a href="https://developers.google.com/appengine/docs/quotas#UrlFetch" target="_blank">GAE URL Fetch Quota</a> for the currency API.
+        <br />And I need your help to enable billing to get larger quota. Do please help to click <a href="http://ko-fi.com?i=8d071856392d304" target="_blank">this link</a> and donate me $1.00USD.</p>
+        <p class="author">--By Hippasus Chu</p>
     </body>
 </html>
         """
