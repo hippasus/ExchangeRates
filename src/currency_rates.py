fcc56e713009f48bd80b314caafc41328fb35131
hippasus <zhuyanggu@gmail.com>
2013-09-01 23:56:36 +0800

    fix issue #8: use unicode string

--- src/currency_rates.py
+++ src/currency_rates.py
@@ -10,35 +10,35 @@ NOT_SUPPORTED_RATE = -1
 class GoogleCurrencyRateRequest():
     def get_rate(self, from_currency, to_currency):
         rate = None
-        url = 'https://www.google.com/ig/calculator?hl=en&q={0}{1}=?{2}'.format(1, from_currency, to_currency)
+        url = u'https://www.google.com/ig/calculator?hl=en&q={0}{1}=?{2}'.format(1, from_currency, to_currency)
         opener = urllib2.build_opener()
         urllib2.install_opener(opener)
 
         # for normal rate, google returns r'{lhs: "1 U.S. dollar",rhs: "21 276.5957 Vietnamese dong",error: "",icc: true}'
         # for small rate, google returns r'{lhs: "1 Vietnamese dong",rhs: "4.7 \x26#215; 10\x3csup\x3e-5\x3c/sup\x3e U.S. dollars",error: "",icc: true}'
-        response_str = urllib2.urlopen(url).read().decode('utf-8', 'ignore')
-        response_str = response_str.decode('string-escape') # decode to r'{lhs: "1 Vietnamese dong",rhs: "4.7 &#215; 10<sup>-5</sup> U.S. dollars",error: "",icc: true}'
+        response_str = urllib2.urlopen(url).read().decode(u'utf-8', u'ignore')
+        response_str = response_str.decode(u'string-escape') # decode to r'{lhs: "1 Vietnamese dong",rhs: "4.7 &#215; 10<sup>-5</sup> U.S. dollars",error: "",icc: true}'
 
-        response_str = response_str.replace('lhs:', '"lhs":')
-        response_str = response_str.replace('rhs:', '"rhs":')
-        response_str = response_str.replace('error:', '"error":')
-        response_str = response_str.replace('icc:', '"icc":')
+        response_str = response_str.replace(u'lhs:', u'"lhs":')
+        response_str = response_str.replace(u'rhs:', u'"rhs":')
+        response_str = response_str.replace(u'error:', u'"error":')
+        response_str = response_str.replace(u'icc:', u'"icc":')
 
         converted = json.loads(response_str)
 
-        pattern = re.compile('^(?P<rate>[\d ]+\.\d+)\s(?:&#215; 10<sup>(?P<exponential>-?[\d]+)</sup> )?')
-        err = converted['error']
+        pattern = re.compile(u'^(?P<rate>[\d ]+\.\d+)\s(?:&#215; 10<sup>(?P<exponential>-?[\d]+)</sup> )?')
+        err = converted[u'error']
         if (err == ''):
-            m = pattern.match(converted['rhs'])
+            m = pattern.match(converted[u'rhs'])
             if (m is not None):
-                rate = float(re.sub(r' ', '', m.group('rate')))
+                rate = float(re.sub(r' ', '', m.group(u'rate')))
                 exponential = 0
-                if m.group('exponential') is not None:
-                    exponential = int(m.group('exponential'))
+                if m.group(u'exponential') is not None:
+                    exponential = int(m.group(u'exponential'))
                 rate = rate * pow(10, exponential)
             else:
                 rate = NOT_SUPPORTED_RATE
-        elif (converted['error'] == '4'):
+        elif (converted[u'error'] == u'4'):
             rate = NOT_SUPPORTED_RATE
 
         return (rate, err)
@@ -53,21 +53,21 @@ class CurrencyRates(webapp2.RequestHandler):
                     v = v.strip()
                 return v
 
-            return (strip(request.get('from')), strip(request.get('to')), strip(request.get('q')), strip(request.get('callback')))
+            return (strip(request.get(u'from')), strip(request.get(u'to')), strip(request.get(u'q')), strip(request.get(u'callback')))
 
         def get_rate(from_currency, to_currency):
-            cache_key = '{0}-{1}'.format(from_currency, to_currency)
+            cache_key = u'{0}-{1}'.format(from_currency, to_currency)
             rate, err = memcache.get(cache_key), None
             if rate is None:
                 req = GoogleCurrencyRateRequest()
                 rate, err = req.get_rate(from_currency, to_currency)
 
-                logging.debug('rate fetched, key is {0}'.format(cache_key))
+                logging.debug(u'rate fetched, key is {0}'.format(cache_key))
 
                 if rate is not None and memcache.add(cache_key, rate, 600):
-                    logging.debug('rate cached, key is {0}'.format(cache_key))
+                    logging.debug(u'rate cached, key is {0}'.format(cache_key))
             else:
-                logging.debug('rate fetched form cache, key is {0}'.format(cache_key))
+                logging.debug(u'rate fetched form cache, key is {0}'.format(cache_key))
 
             return rate, err
 
@@ -77,20 +77,20 @@ class CurrencyRates(webapp2.RequestHandler):
             rate, err = get_rate(from_currency, to_currency)
 
             if rate is None:
-                result = {"err": err}
+                result = {u'err': err}
             elif rate == NOT_SUPPORTED_RATE:
-                result = {"err": 'not supported rate conversion.'}
+                result = {u'err': u'not supported rate conversion.'}
             else:
-                result = {"from": from_currency, "to": to_currency, "rate": rate }
+                result = {u'from': from_currency, u'to': to_currency, u'rate': rate }
 
                 if (qty is not None and len(qty) > 0):
                     try:
                         qty = float(qty)
                         converted_qty = qty * rate
-                        result["v"] = converted_qty
+                        result[u'v'] = converted_qty
                     except:
-                        result["warning"] = "invalid quantity, ignored."
+                        result[u'warning'] = u'invalid quantity, ignored.'
         else:
-            result = {"err": "invalid request"}
+            result = {u'err': u'invalid request'}
 
         utils.write_jsonp_output(response, result, callback)
